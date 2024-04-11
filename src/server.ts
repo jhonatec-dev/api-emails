0a44dbd47f030fedaad189480e66ff2986ed7e6b
Jhonatec <jhonsreis@gmail.com>
2024-04-11 08:36:47 -0300

    feat: connecting with MONGODB

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,45 @@
+import cors from 'cors'
 import dayjs from 'dayjs'
+import { configDotenv } from 'dotenv'
+import express, { type Application } from 'express'
+import mongoose from 'mongoose'
 
-const greeting = 'Hello, World'
-const today = dayjs().format('DD/MM/YYYY')
+configDotenv()
 
-console.log(`${today}: ${greeting}`)
+const password = process.env.MONGO_PASSWORD
+const user = process.env.MONGO_USER
+const collection = process.env.MONGO_COLLECTION
+const uri = encodeURI(
+  `mongodb+srv://${user}:${password}@cluster0.y63pkr8.mongodb.net/${collection}?retryWrites=true&w=majority`
+)
+const port = process.env.PORT
+
+const today = dayjs().format('DD/MM/YYYY HH:mm:ss')
+
+const app: Application = express()
+
+async function startServer (): Promise<void> {
+  try {
+    await mongoose.connect(uri, {
+      dbName: collection
+    })
+
+    app.use(express.json())
+    app.use(cors())
+    // app.use(router)
+
+    app.get('/', (_req, res) => {
+      res.send('Hello World!')
+    })
+
+    app.listen(port, () => {
+      console.log(`${today} - Servidor rodando na porta ${port}`)
+    })
+  } catch (error) {
+    console.log(today, 'Erro no servidor MONGO:', error)
+  }
+}
+
+startServer().catch((error) => {
+  console.error(today, 'Erro no servidor:', error)
+})
